05569ce471fc28ccafe4509ac077eb57728aca55
aman gardas <aman gardas@DESKTOP-AQAT1N6>
2022-02-04 12:56:44 +0530

    com.project added in the project 02-02-2022

---
+++ 02-02-2022/src/com/project/Example2.java
@@ -0,0 +1,25 @@
+package com.project;
+
+public class Example2
+{
+
+	public static void main(String[] args)
+	{ 
+		int learnerId = 4949;
+		String learnerName = "Hema";
+		int learnerAge = 21;
+		float learnerMarks = 78.50f;
+		String learnerAddress = "Hyderabad";
+		System.out.print("Learner Id:");
+		System.out.println(learnerId);
+		System.out.println("Learner Name:");
+		System.out.println(learnerName);
+		System.out.println("Learner Age:");
+		System.out.println(learnerAge);
+		System.out.println("Learner Marks:");
+		System.out.println(learnerMarks);
+		System.out.println("Learner Address:");
+		System.out.println(learnerAddress);
+	}
+
+}
